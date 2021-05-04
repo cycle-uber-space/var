8c8353dc28031ebf880b2778dd1a4af065687d3b
Cycle <cycle@uber.space>
2021-05-04 12:44:09 +0200

    add string type

--- var.hpp
+++ var.hpp
@@ -2,11 +2,20 @@
 #ifndef _VAR_HPP_
 #define _VAR_HPP_
 
+#ifndef VAR_WANT_STRING
+#define VAR_WANT_STRING 1
+#endif
+
 #ifndef VAR_FAIL
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 #define VAR_FAIL(...) do { fprintf(stderr, "FAIL: "); fprintf(stderr, __VA_ARGS__); raise(SIGINT); exit(1); } while (0)
+#endif
+
+#if VAR_WANT_STRING
+#include <stdlib.h>
+#include <string.h>
 #endif
 
 #include <stdint.h>
@@ -24,18 +33,76 @@ typedef int64_t I64;
 typedef float F32;
 typedef double F64;
 
+#if VAR_WANT_STRING
+
+class str
+{
+public:
+    str() : _size(0),  _data(nullptr)
+    {
+    }
+
+    str(char const * str)
+    {
+        _size = strlen(str) + 1;
+        _data = (char *) malloc(_size);
+        memcpy(_data, str, _size);
+    }
+
+    str(str const & other)
+    {
+        _size = other._size;
+        _data = (char *) malloc(_size);
+        memcpy(_data, other._data, _size);
+    }
+
+    ~str()
+    {
+        if (_data)
+        {
+            _size = 0;
+            free(_data);
+            _data = nullptr;
+        }
+    }
+
+    str & operator=(str const & other)
+    {
+        if (_data)
+        {
+            free(_data);
+        }
+        _size = other._size;
+        _data = (char *) malloc(_size);
+        memcpy(_data, other._data, _size);
+        return *this;
+    }
+
+    size_t _size;
+    char * _data;
+};
+
+#endif
+
 class var
 {
 public:
     var();
     var(F32 val);
     var(F64 val);
+#if VAR_WANT_STRING
+    var(char const * str);
+#endif
 
     bool is_nil() const;
     bool is_f32() const;
     bool is_f64() const;
+#if VAR_WANT_STRING
+    bool is_str() const;
+#endif
 
     F32 to_f32() const;
+    char const * to_str() const;
 
     int _type;
     union
@@ -43,6 +110,10 @@ public:
         F32 f32;
         F64 f64;
     } _data;
+#if VAR_WANT_STRING
+    // NOTE we cannot put types with non-trivial constructors into unions
+    str _data_str;
+#endif
 };
 
 #endif
@@ -57,6 +128,9 @@ enum
     VAR_NIL = 0,
     VAR_F32,
     VAR_F64,
+#if VAR_WANT_STRING
+    VAR_STR,
+#endif
 };
 
 static char const * type_name(int type)
@@ -69,6 +143,10 @@ static char const * type_name(int type)
         return "f32";
     case VAR_F64:
         return "f64";
+#if VAR_WANT_STRING
+    case VAR_STR:
+        return "str";
+#endif
     default:
         return "<unknown>";
     };
@@ -88,6 +166,15 @@ var::var(F64 val) : _type(VAR_F64)
     _data.f64 = val;
 }
 
+#if VAR_WANT_STRING
+
+var::var(char const * str) : _type(VAR_STR)
+{
+    _data_str = str;
+}
+
+#endif
+
 bool var::is_nil() const
 {
     return _type == VAR_NIL;
@@ -103,6 +190,15 @@ bool var::is_f64() const
     return _type == VAR_F64;
 }
 
+#if VAR_WANT_STRING
+
+bool var::is_str() const
+{
+    return _type == VAR_STR;
+}
+
+#endif
+
 F32 var::to_f32() const
 {
     switch (_type)
@@ -116,6 +212,20 @@ F32 var::to_f32() const
         return F32();
     }
 };
+
+#if VAR_WANT_STRING
+
+char const * var::to_str() const
+{
+    switch (_type)
+    {
+    default:
+        VAR_FAIL("no conversion %s() for type %s (%d)\n", __FUNCTION__, type_name(_type), _type);
+        return NULL;
+    }
+}
+
+#endif
 
 #endif
 
