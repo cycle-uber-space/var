d28ce9a92a7f89679abb64c2507503e9e3b5237c
Cycle <cycle@uber.space>
2021-05-04 12:22:33 +0200

    add standard typedefs

--- var.hpp
+++ var.hpp
@@ -2,6 +2,21 @@
 #ifndef _VAR_HPP_
 #define _VAR_HPP_
 
+#include <stdint.h>
+
+typedef uint8_t U8;
+typedef uint16_t U16;
+typedef uint32_t U32;
+typedef uint64_t U64;
+
+typedef int8_t I8;
+typedef int16_t I16;
+typedef int32_t I32;
+typedef int64_t I64;
+
+typedef float F32;
+typedef double F64;
+
 #endif
 
 #ifdef VAR_IMPLEMENTATION
