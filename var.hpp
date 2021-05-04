00e48fb5e13f5419f61e2e3b8cc5f757312b74ee
Cycle <cycle@uber.space>
2021-05-04 12:26:13 +0200

    add initial var class

--- var.hpp
+++ var.hpp
@@ -17,12 +17,36 @@ typedef int64_t I64;
 typedef float F32;
 typedef double F64;
 
+class var
+{
+public:
+    var();
+
+    bool is_nil() const;
+
+    int _type;
+};
+
 #endif
 
 #ifdef VAR_IMPLEMENTATION
 
 #ifndef _VAR_CPP_
 #define _VAR_CPP_
+
+enum
+{
+    VAR_NIL = 0,
+};
+
+var::var() : _type(VAR_NIL)
+{
+}
+
+bool var::is_nil() const
+{
+    return _type == VAR_NIL;
+}
 
 #endif
 
